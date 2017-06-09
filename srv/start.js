a3845b4164c43238b755a45c1062584308890a1c
Vaughan Nickerson <vaughan.nickerson@parivedasolutions.com>
2017-06-08 22:28:44 -0400

    OG commit

--- srv/start.js
+++ srv/start.js
@@ -1,6 +1,17 @@
 const Seneca = require('seneca');
-const 
+const Feedback = require('../index');
 
-const Service = Seneca
+const Service = Seneca()
     .listen({ port: 5000, host: '127.0.0.1' })
-    .use();
+    .use(Feedback);
+
+Service.ready((err) => {
+    if (err) {
+        return Service.log.error(err);
+    }
+
+    return Service.log.info({
+        service: 'feedback-service',
+        message: 'Feedback service started'
+    });
+});
